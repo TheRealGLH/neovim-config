b15c7335671eaec4fd646fbb59817c0da0a3ea17
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-02-20 14:47:31 +0100

    None-LS

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -1,15 +1,15 @@
 return {
     {
-    "williamboman/mason.nvim",
-    config = function() 
-        require("mason").setup()
-    end
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls","tsserver"}
+                ensure_installed = { "lua_ls", "tsserver" }
             })
         end
     },
@@ -22,6 +22,7 @@ return {
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
             vim.keymap.set('n', '<space>f', function()
                 vim.lsp.buf.format { async = true }
             end, {})

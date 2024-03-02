453639f62d8811e1c4986f8a0cd5b71b969c5682
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-03-02 18:25:55 +0100

    Adds extra treesitter required installs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup( {
             auto_install = true,
-            ensure_installed = {"lua", "javascript", "css", "typescript", "java", "c_sharp", "cpp", "scss", "json"},
+            ensure_installed = {"lua", "javascript", "css", "typescript", "java", "c_sharp", "cpp", "scss", "json", "gitignore", "html", "javascript", "xml", "yaml", "angular", "vue"},
             highlight = { enable = true},
             indent = { enable = true }
         })

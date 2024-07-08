4ec042c1d334946781d004b2c58a40cc6fa6f9ec
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-07-08 16:35:36 +0200

    volar lsp for VueJS

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "angularls", "html", "jdtls", "csharp_ls"}
+                ensure_installed = { "lua_ls", "tsserver", "angularls", "html", "jdtls", "csharp_ls", "volar"}
             })
         end
     },
@@ -20,6 +20,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
             lspconfig.csharp_ls.setup({})
+            lspconfig.volar.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})

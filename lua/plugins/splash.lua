ef4c8c552385fd773596740cba3b4632d8999eb1
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-07-10 20:29:01 +0200

    complete splash options

--- lua/plugins/splash.lua
+++ lua/plugins/splash.lua
@@ -16,7 +16,7 @@ return {
                 disable_move = true,
                center = {
                     {
-                        icon = ' 󰱼 ',
+                        icon = ' 󰈔 \t',
                         icon_hl = 'DashboardIcon',
                         desc = 'Open file',
                         desc_hl = 'DashboardDesc',
@@ -26,39 +26,81 @@ return {
                         action = 'Telescope find_files',
                     },
                     {
-                        icon = '  ',
+                        icon = ' 󰈞 \t',
+                        icon_hl = 'Title',
+                        desc = 'Fuzzy Grep',
+                        desc_hl = 'DashboardDesc',
+                        key = '2',
+                        key_hl = 'Number',
+                        key_format = ' %s.', -- `%s` will be substituted with value of `key`
+                        action = 'Telescope live_grep',
+                    },
+                    {
+                        icon = ' 󰂺 \t',
                         icon_hl = 'Title',
                         desc = 'Open Readme',
                         desc_hl = 'DashboardDesc',
-                        key = '2',
+                        key = '3',
                         key_hl = 'Number',
                         key_format = ' %s.', -- `%s` will be substituted with value of `key`
                         action = 'Glow README.md',
                     },
                     {
-                        icon = '  ',
+                        icon = '  \t',
+                        icon_hl = 'DashboardIcon',
+                        desc = 'Git History',
+                        desc_hl = 'DashboardDesc',
+                        key = '4',
+                        key_hl = 'Number',
+                        key_format = ' %s.', -- `%s` will be substituted with value of `key`
+                        action = 'Telescope git_commits',
+                    },
+                    {
+                        icon = '  \t',
                         icon_hl = 'Title',
                         desc = 'Mason Config',
                         desc_hl = 'DashboardDesc',
-                        key = '3',
+                        key = '5',
                         key_hl = 'Number',
                         key_format = ' %s.', -- `%s` will be substituted with value of `key`
                         action = 'Mason',
                     },
                     {
-                        icon = ' 󰊢 ',
-                        icon_hl = 'DashboardIcon',
-                        desc = 'Git History',
+                        icon = '  \t',
+                        icon_hl = 'Title',
+                        desc = 'LazyVim Config',
                         desc_hl = 'DashboardDesc',
-                        key = '1',
+                        key = '6',
                         key_hl = 'Number',
                         key_format = ' %s.', -- `%s` will be substituted with value of `key`
-                        action = 'Telescope git_status',
+                        action = 'Lazy',
+                    },
+                    {
+                        icon = '  \t',
+                        icon_hl = 'Title',
+                        desc = 'Vim Options',
+                        desc_hl = 'DashboardDesc',
+                        key = '7',
+                        key_hl = 'Number',
+                        key_format = ' %s.', -- `%s` will be substituted with value of `key`
+                        action = 'Telescope vim_options',
+                    },
+                    {
+                        icon = ' 󰈆 \t',
+                        icon_hl = 'Title',
+                        desc = 'Quit NeoVim',
+                        desc_hl = 'DashboardDesc',
+                        key = '0',
+                        key_hl = 'Number',
+                        key_format = ' %s.', -- `%s` will be substituted with value of `key`
+                        action = 'qa',
                     },
                 }
 
             }
         }
-  end,
+  end
+,
+
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }

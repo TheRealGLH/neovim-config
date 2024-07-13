7fca5150f4a188d935009118ddc2c6efbb05220c
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-07-13 02:07:44 +0200

    aaaaaa why did they just happen to pick the same default keymap I use for something else in neotree 3.0.whatever

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,15 +7,18 @@ return {
         'MunifTanjim/nui.nvim',
     },
     config = function()
-        hijack_netrw_behavior = "open_default",
+        hijack_netrw_behavior = "open_default"
 --        vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
-        vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>')
+        vim.keymap.set('n', '<C-b>', ':Neotree toggle position=left<CR>')
         vim.keymap.set('n', '<C-M-b>', ':Neotree toggle position=right source=git_status<CR>')
         require('neo-tree').setup({
             hijack_netrw_behavior = "disabled",
             close_if_last_window = true,
             window = {
                 position = "left",
+                mappings = {
+                    ["<C-b>"] = "noop",--this normally scrolls backwards through previews... but so does the backspace key. and it messes with my preferred shortcut.
+                }
             }
         })
         --vim.cmd('Neotree')

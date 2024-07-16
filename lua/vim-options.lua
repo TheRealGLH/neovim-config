afe148a072e89b4ca921d53e1d08313f570e4c2a
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-07-16 12:13:01 +0200

    relative number + number type toggle

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,8 +3,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+vim.cmd("set number relativenumber")
 vim.cmd("set mouse=")
 vim.g.mapleader = " "
 --vim.opt.mouse = ""
-
+vim.keymap.set('n', '<leader>n', ':set number relativenumber!<CR>', {})

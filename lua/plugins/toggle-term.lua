5416629e38d3c29184d916bb9860df464f319997
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-07-28 20:32:04 +0200

    Powershell and shuckle updated

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,7 +1,23 @@
 return {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = {
-      open_mapping = [[<c-j>]],  --blah
-    },
+    config = function ()
+        if jit.os == 'Windows' then
+            local powershell_options = {
+                shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+                shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+                shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+                shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+                shellquote = "",
+                shellxquote = "",
+            }
+            for option, value in pairs(powershell_options) do 
+                vim.opt[option] = value
+            end
+        end
+        require('toggleterm').setup {
+            open_mapping = [[<c-j>]],
+            direction = 'float',
+        }
+    end
 }

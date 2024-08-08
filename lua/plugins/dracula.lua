959efeb25583f5ff91da7fa2eb0959c32445f54e
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-08-08 14:28:16 +0200

    transparent bg

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,8 +1,12 @@
-return { 
-    "Mofiqul/dracula.nvim", 
-    name = "dracula", 
+return {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
     priority = 1001,
     config = function()
+        require('dracula').setup {
+            transparent_bg = true
+
+        }
         vim.cmd.colorscheme "dracula"
     end
 }

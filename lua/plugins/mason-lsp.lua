98720fe8c51c03d8e4fc485c606630ee0704df34
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-10-21 12:13:57 +0200

    rust analyzer mason

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "angularls", "html", "jdtls", "csharp_ls", "vuels", "clangd"}
+                ensure_installed = { "lua_ls", "tsserver", "angularls", "html", "jdtls", "csharp_ls", "vuels", "clangd", "rust_analyzer"}
             })
         end
     },
@@ -22,6 +22,7 @@ return {
             lspconfig.csharp_ls.setup({})
             lspconfig.vuels.setup({})
             lspconfig.clangd.setup({})
+            lspconfig.rust_analyzer.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})

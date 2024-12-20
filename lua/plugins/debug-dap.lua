5e6f3f626713bc233bc1a27e90a0e0f1145dc2de
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2024-12-20 09:25:48 +0100

    gdb rust config for dap

--- lua/plugins/debug-dap.lua
+++ lua/plugins/debug-dap.lua
@@ -23,6 +23,23 @@ return {
             command = jit.os == 'Windows' and 'netcoredbg' or '/user/local/bin/netcorebg',
             args = { '--interpreter=vscode' }
         }
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+        };
+        dap.configurations.rust = {
+            {
+                name = "Launch",
+                type = "gdb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            }
+        }
         dap.configurations.cs = {
             {
                 type = "coreclr",

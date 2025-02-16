e2f2e0c3387f662a9f2847d56c231f72d4d24c75
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2025-02-16 15:13:41 +0100

    updates telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ local actions = require("telescope.actions")
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
+        tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
             config = function()

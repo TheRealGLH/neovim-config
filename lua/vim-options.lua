7b835bbc1ec3a81828b43795bdaa817edd476d1b
TheRealGLH <TheRealGLH@users.noreply.github.com>
2025-04-25 16:39:42 +0200

    Disables guicursor entirely

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,10 +10,10 @@ vim.g.mapleader = " "
 vim.g.health = { style = 'float' }
 vim.keymap.set('n', '<leader>n', ':set number relativenumber!<CR>', {})
 vim.opt.showmode = false
+vim.opt.guicursor=""
 vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
-  set guicursor=n-v-c-sm:block,i-ci-ve:hor35,r-cr-o:hor20
 ]]
